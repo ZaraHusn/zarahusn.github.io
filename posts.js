afa96f8b12739dad53afd95c4831ac811599da67
Zara Hussain <148576241+ZaraHusn@users.noreply.github.com>
2026-09-04 22:42:42 +0100

    Update posts.js

--- posts.js
+++ posts.js
@@ -20,4 +20,11 @@ const posts = [
     read: "7 min read",
     tags: ["trading", "math", "finance"]
   }
+  {
+  slug: "trial",
+    title: "Mathematics of Trading: Modeling Randomness",
+    date: "2025-11-10",
+    read: "7 min read",
+    tags: ["trading", "math", "finance"]
+  }
 ];
