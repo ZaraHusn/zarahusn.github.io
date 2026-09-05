577b127ff45b7c9bd1d176fdd1ea5766cbf92cf6
Zara Hussain <148576241+ZaraHusn@users.noreply.github.com>
2026-09-05 21:39:28 +0100

    Update posts.js

--- posts.js
+++ posts.js
@@ -2,6 +2,7 @@ const posts = [
   {
     slug: "mathematics-of-trading-introduction",
     title: "Mathematics of Trading: Introduction",
+    section: "Fixed Income",
     date: "2025-10-10",
     read: "3 min read",
     tags: ["trading", "math", "finance"]
@@ -9,6 +10,7 @@ const posts = [
   {
     slug: "mathematics-of-trading-uncertainty",
     title: "Mathematics of Trading: Uncertainty",
+    section: "Mathematics of Trading",
     date: "2025-10-12",
     read: "5 min read",
     tags: ["trading", "math", "finance"]
@@ -16,6 +18,7 @@ const posts = [
   {
     slug: "mathematics-of-trading-modeling-randomness",
     title: "Mathematics of Trading: Modeling Randomness",
+    section: "Math-Stats",
     date: "2025-11-10",
     read: "7 min read",
     tags: ["trading", "math", "finance"]
